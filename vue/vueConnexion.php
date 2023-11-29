ddec3cbe93c900ca7795216b32c8873422a55bc7
CrazyFire59 <louisbocq59@gmail.com>
2023-11-29 13:38:22 +0100

    Co-authored-by: David DELAVA <david.delava@gastonberger.fr>
    undefined

---
+++ vue/vueConnexion.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Connexion</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="icon" type="image/png" href="../images/CashCash.png" />
+    </head>
+    <body>
+        <img class="logo" src="../images/CashCash.png" alt="logo">
+        <h1>Connexion</h1>
+        <!--<img class="line1" src="../images/Line 4.png" alt="ligne"> -->
+        <img class="line2" src="../images/Line 3.png" alt="ligne"> <br>
+        <form action="" method="post">
+            <p>
+                <label for="login">Login</label>
+                <input type="text" name="login" id="login">
+            </p>
+            <p>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password">
+            </p>
+            <p>
+                <input type="submit" value="Se connecter">
+            </p>
+        </form>
+    </body>
+</html>
