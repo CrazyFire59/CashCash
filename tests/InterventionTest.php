c06a863714a81ba16095c8d748417541920e3d2b
maxence castel <maxence.castel@sio.lan>
2024-04-10 13:52:05 +0200

    Revert "test getInterventionById intervention_id"
    This reverts commit 85c100678c13393a199d4a3140581700fb93092b.

--- tests/InterventionTest.php
+++ tests/InterventionTest.php
@@ -7,15 +7,8 @@ class InterventionTest extends TestCase
 {
 
     public function testIntervention(){
-        $Intervention = new Intervention();
-        $this->assertEquals("abc", $Intervention->interventiontest());
-    }
-
-    public function testGetInterventionById(){
-        $Intervention = new Intervention();
-
-        $intervention = $Intervention->getInterventionById(1);
-        $this->assertEquals(2, $intervention["intervention_id"]);
+        $intervention = new Intervention();
+        $this->assertEquals("abc", $intervention->interventiontest());
     }
 
 }
