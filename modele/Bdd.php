77192630dc82e2d1c510d48aae440e16713e5f1c
CrazyFire59 <louisbocq59@gmail.com>
2024-04-23 13:23:27 +0200

    sync

--- modele/Bdd.php
+++ modele/Bdd.php
@@ -12,29 +12,9 @@ class Bdd {
             return $bdd;
         } catch (PDOException $e) {
             echo "Erreur : " . $e->getMessage();
-            // Pour arrêter l'exécution en cas d'erreur de connexion :
-            // die("Erreur de connexion : " . $e->getMessage());
-            return null; // Retourne null en cas d'erreur
+            return null; 
         }
     }
-
-    // private $_dbName = "cashcash";
-    // private $_dbUser = "root";
-    // private $_dbPwd = "";
-
-    // public function connexionPDO(){
-    //     try {
-    //         $dsn = 'mysql:host=127.0.0.1;dbname=' . $this->_dbName . ';charset=utf8';
-    //         $bdd = new PDO($dsn, $this->_dbUser, $this->_dbPwd);
-    //         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //         return $bdd;
-    //     } catch (PDOException $e) {
-    //         echo "Erreur : " . $e->getMessage();
-    //         // Pour arrêter l'exécution en cas d'erreur de connexion :
-    //         // die("Erreur de connexion : " . $e->getMessage());
-    //         return null; // Retourne null en cas d'erreur
-    //     }
-    // }
 }
 
 ?>
