ae5a6552e720448dda487a7c5b2da3ec141b9621
CrazyFire59 <louisbocq59@gmail.com>
2024-04-25 16:35:08 +0200

    BDD remarche

--- modele/Bdd.php
+++ modele/Bdd.php
@@ -1,20 +1,20 @@
 <?php
 class Bdd {
     /* bdd hebergée */
-    // private $_dbName = "CashCash";
-    // private $_dbUser = "CashCash";
-    // private $_dbPwd = "DT56gK_*p%-3ol2H";
+    private $_dbName = "CashCash";
+    private $_dbUser = "CashCash";
+    private $_dbPwd = "DT56gK_*p%-3ol2H";
     /* en local */
-    private $_dbName = "cashcash";
-    private $_dbUser = "root";
-    private $_dbPwd = "";
+    //private $_dbName = "cashcash";
+    //private $_dbUser = "root";
+    //private $_dbPwd = "";
 
     public function connexionPDO(){
         try {
             /* bdd hebergée */
-            // $dsn = 'mysql:host=185.207.226.14:3306;dbname=' . $this->_dbName . ';charset=utf8';
+            $dsn = 'mysql:host=185.207.226.14:3306;dbname=' . $this->_dbName . ';charset=utf8';
             /* en local */
-            $dsn = 'mysql:host=127.0.0.1;dbname=' . $this->_dbName . ';charset=utf8';
+            // $dsn = 'mysql:host=127.0.0.1;dbname=' . $this->_dbName . ';charset=utf8';
             $bdd = new PDO($dsn, $this->_dbUser, $this->_dbPwd);
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $bdd;
